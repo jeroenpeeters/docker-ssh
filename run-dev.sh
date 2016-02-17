6361d48b76cfefe15313602e4a2d96d3403a6f00
jeroenpeeters <jeroen@peetersweb.nl>
2016-02-17 11:06:54 +0100

    run-dev.sh: nodemon watch coffee files

--- run-dev.sh
+++ run-dev.sh
@@ -2,4 +2,4 @@
 
 docker run --name ubuntu -t -d ubuntu bash
 
-CONTAINER=ubuntu CONTAINER_SHELL=bash KEYPATH=id_rsa PORT=2222 HTTP_PORT=8022 AUTH_MECHANISM=noAuth nodemon server.coffee | bunyan
+CONTAINER=ubuntu CONTAINER_SHELL=bash KEYPATH=id_rsa PORT=2222 HTTP_PORT=8022 AUTH_MECHANISM=noAuth nodemon -e coffee server.coffee | bunyan
