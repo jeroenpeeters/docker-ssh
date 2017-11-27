c571a8e3725a2e82540d96bd1cc41bbfa1cd4678
jeroenpeeters <jeroen@peetersweb.nl>
2017-11-27 14:33:30 +0100

    new dev run example script based on docker filters

--- run-dev.sh
+++ run-dev.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-docker run --name ubuntu -t -d ubuntu bash
+docker run --name ubuntu -t -d --label label1=value1 ubuntu bash
 
-CONTAINER=ubuntu CONTAINER_SHELL=bash KEYPATH=id_rsa PORT=2222 HTTP_PORT=8022 AUTH_MECHANISM=noAuth nodemon -e coffee server.coffee | bunyan
+CONTAINER=ubuntu CONTAINER_SHELL=bash KEYPATH=id_rsa PORT=2222 HTTP_PORT=8022 \
+  AUTH_MECHANISM=simpleAuth \
+  AUTH_USER=myuser \
+  AUTH_PASSWORD=1234 \
+  FILTERS={\"label\":[\"label1=value1\"]} \
+  nodemon -e coffee server.coffee | bunyan
 
 docker stop ubuntu && docker rm --force ubuntu
