8e2a04a4e87d068d2dc36b5910d156b3fd361406
jeroenpeeters <jeroen@peetersweb.nl>
2017-11-27 15:15:54 +0100

    new dev run example script based on docker filters

--- run-dev.sh
+++ run-dev.sh
@@ -2,7 +2,7 @@
 
 docker run --name ubuntu -t -d --label label1=value1 ubuntu bash
 
-CONTAINER=ubuntu CONTAINER_SHELL=bash KEYPATH=id_rsa PORT=2222 HTTP_PORT=8022 \
+CONTAINER_SHELL=bash KEYPATH=id_rsa PORT=2222 HTTP_PORT=8022 \
   AUTH_MECHANISM=simpleAuth \
   AUTH_USER=myuser \
   AUTH_PASSWORD=1234 \
